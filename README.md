887c13c65481843225795ce43a8a59eed8776d79
Madhan-sai-Au46 <itsmadhan98@gmail.com>
2023-02-04 00:47:57 +0530

    entire react

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # COMPLETE_REACT
 REACT ALL CONCEPTS ARE WRITTEN HERE
+MUST INSTALL * npm install react-router-dom
+* npm install axios
---
+++ complete/src/Components/Router/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+
+
+const Home = () => {
+  return (
+    <div className='routercss'><h2>this is Home Page</h2></div>
+  )
+}
+
+export default Home
